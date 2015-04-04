b78f03ba48573e550a9c40514d786f8e253251c8
annemarie35 <annemariee@free.fr>
2015-04-04 14:46:21 +0200

    Added more views to the website

--- makeit/settings.py
+++ makeit/settings.py
@@ -111,3 +111,23 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
